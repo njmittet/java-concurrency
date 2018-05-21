6cfff3bd570705127e7d1583a7018304cc76381a
Nils Jørgen Mittet <njmittet@gmail.com>
2018-05-21 22:37:39 +0200

    Add CompletableFuture example

--- src/main/java/no/njm/concurrency/Application.java
+++ src/main/java/no/njm/concurrency/Application.java
@@ -1,5 +1,47 @@
 package no.njm.concurrency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.completableFutures();
+        waitBeforeExit();
+    }
+
+    private void completableFutures() {
+        // Chaining callbacks. supplyAsync() is used when the result should be used.
+
+        CompletableFuture.supplyAsync(() -> 5 * 2)
+                         .thenApply(i -> i * 2)
+                         .thenAccept(i -> log.debug("Calculated i to be {}.", i))
+                         .thenRun(() -> log.debug("CompletableFuture finished."));
+
+        // All completion stages run in the same thread unless specified. runAsync() does not
+        // care about the result.
+        CompletableFuture.runAsync(() -> log.info("Thread: {}", Thread.currentThread().getName()))
+                         .thenRun(() -> log.debug("Running in same thread: {}", Thread.currentThread().getName()))
+                         .thenRunAsync(() -> log.debug("Can run in new thread: {}", Thread.currentThread().getName()));
+
+        // Completable futures can be composed and combined.
+        CompletableFuture.supplyAsync(() -> "Hello")
+                         .thenCompose(s -> CompletableFuture
+                                 .supplyAsync(() -> s + " World"))
+                         .thenAccept(log::debug);
+
+    }
+
+    private static void waitBeforeExit() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            log.error("Thread interrupted.", e);
+        }
+    }
 }
--- src/test/java/no/njm/concurrency/ApplicationTest.java
+++ src/test/java/no/njm/concurrency/ApplicationTest.java
@@ -2,12 +2,9 @@ package no.njm.concurrency;
 
 import org.junit.Before;
 
-import static org.junit.Assert.*;
-
 public class ApplicationTest {
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
     }
-
 }
