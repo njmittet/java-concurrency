1cb6d9a8942909580f672929bd9a23a392d9030a
Nils Jørgen Mittet <njmittet@gmail.com>
2018-05-21 22:46:37 +0200

    Update Gradle wrapper

--- src/main/java/no/njm/concurrency/Application.java
+++ src/main/java/no/njm/concurrency/Application.java
@@ -17,7 +17,6 @@ public class Application {
 
     private void completableFutures() {
         // Chaining callbacks. supplyAsync() is used when the result should be used.
-
         CompletableFuture.supplyAsync(() -> 5 * 2)
                          .thenApply(i -> i * 2)
                          .thenAccept(i -> log.debug("Calculated i to be {}.", i))
@@ -35,6 +34,52 @@ public class Application {
                                  .supplyAsync(() -> s + " World"))
                          .thenAccept(log::debug);
 
+        // Calling a method when two different CompletableFutures completes.
+        CompletableFuture<Customer> customerFuture = loadCustomer(1);
+        CompletableFuture<Shop> shopFuture = findShop("Oslo");
+        customerFuture.thenCombine(shopFuture, this::findRoute)
+                      .thenRun(() -> log.debug("Routing complete."));
+
+        customerFuture.thenAcceptBoth(shopFuture, (cust, shop) -> {
+            final Route route = findRoute(cust, shop);
+            //refresh GUI with route
+        });
+
+        // Catching exceptions asynchronously.
+        CompletableFuture.supplyAsync(() -> 2 / 0)
+                         .thenAccept(i -> log.debug("Result is: {}", i))
+                         .exceptionally(e -> {
+                             log.error("Divide by zero caused exception.", e);
+                             return null;
+                         });
+
+        // A more flexible approach is handle() that takes a function receiving either correct
+        // result or exception. handle() is called always, with either result or exception argument
+        // being not-null. This is a one-stop catch-all strategy.
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+        completableFuture.completeExceptionally(new RuntimeException());
+        completableFuture.handle((ok, e) -> {
+            if (ok != null) {
+                return Integer.parseInt(ok);
+            } else {
+                log.error("handle() caught exception.", e);
+                return -1;
+            }
+        });
+
+    }
+
+    private Route findRoute(Customer customer, Shop shop) {
+        log.debug("Called with customer:{} and shop:{}", customer, shop);
+        return new Route();
+    }
+
+    private CompletableFuture<Customer> loadCustomer(int customerId) {
+        return CompletableFuture.completedFuture(new Customer(customerId));
+    }
+
+    private CompletableFuture<Shop> findShop(String city) {
+        return CompletableFuture.completedFuture(new Shop(city));
     }
 
     private static void waitBeforeExit() {
@@ -43,5 +88,27 @@ public class Application {
         } catch (InterruptedException e) {
             log.error("Thread interrupted.", e);
         }
+    }
+
+    private static class Customer {
+
+        private final int customerId;
+
+        Customer(int customerId) {
+            this.customerId = customerId;
+        }
+    }
+
+    private static class Shop {
+
+        private final String shopName;
+
+        Shop(String shopName) {
+            this.shopName = shopName;
+        }
+    }
+
+    private static class Route {
+
     }
 }
--- src/test/java/no/njm/concurrency/ApplicationTest.java
+++ src/test/java/no/njm/concurrency/ApplicationTest.java
@@ -1,10 +1,29 @@
 package no.njm.concurrency;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.Assert.*;
 
 public class ApplicationTest {
 
     @Before
     public void setUp() {
+    }
+
+    @Test
+    public void completedFutureExample() {
+        CompletableFuture<String> cf = CompletableFuture.completedFuture("Message");
+        assertTrue(cf.isDone());
+        assertEquals("Message", cf.getNow(null));
+    }
+
+    @Test
+    public void incompleteFutureExample() {
+        CompletableFuture<String> cf = new CompletableFuture<>();
+        assertTrue(!cf.isDone());
+        assertNull(cf.getNow(null));
     }
 }
